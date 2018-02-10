68a472b557420a3e5263e81fb93663edb91a0d00
bijuneyyan <bijuneyyan@gmail.com>
2018-02-10 19:31:15 +0900

    Update app.js

--- app.js
+++ app.js
@@ -8,14 +8,41 @@ let express = require('express');
 let bodyParser = require('body-parser');
 
 let app = express();
-app.use(bodyParser.json({type: 'application/json'}));
+app.use(bodyParser.json());
 
-const getCompanyIntent = 'getCompany';
+const getCompanyIntent = 'get_Company';
 const companyArgument = 'company';
 const marketsDataKey = process.env.markets;
 const sessionIds = {};
 
-app.post('/', function (req, res) {
+app.get('/webhook', function (req, res) {
+  res.send('Use the /webhook endpoint.')
+})
+
+
+app.post('/webhook', function (req, res) {
+  // we expect to receive JSON data from api.ai here.
+  // the payload is stored on req.body
+  console.log(req.body)
+
+
+  // the value of Action from api.ai is stored in req.body.result.action
+  console.log('* Received action -- %s', req.body.result.action)
+
+  // parameters are stored in req.body.result.parameters
+  var webhookReply = 'Hello from the webhook.'
+
+  // the most basic response
+  res.status(200).json({
+    source: 'webhook',
+    speech: webhookReply,
+    displayText: webhookReply
+  })
+});
+
+
+/*
+app.post('/webhook', function (req, res) {
   const thisSessionID = req.body.sessionId;
   if (sessionIds[thisSessionID] === undefined ){
       sessionIds[thisSessionID] = [thisSessionID];
@@ -29,12 +56,13 @@ app.post('/', function (req, res) {
   console.log ('EXTRACTED_SessionId=' , req.body.sessionId);
   console.log ('EXTRACTED_conversation_id=' , req.body.originalRequest.data.conversation.conversation_id);
   const assistant = new Assistant({request: req, response: res});
+  
   function getCompany (assistant) {
   
     
-  /* let company = assistant.getArgument(companyArgument);
+  let company = assistant.getArgument(companyArgument);
 
-  fetch(`http://markets.ft.com/research/webservices/securities/v1/search?query=${company}&source=${marketsDataKey}`).then((data) => {
+  /* fetch(`http://markets.ft.com/research/webservices/securities/v1/search?query=${company}&source=${marketsDataKey}`).then((data) => {
     if (data.ok) {
         return data.json();
       }
@@ -50,11 +78,17 @@ app.post('/', function (req, res) {
     console.log(error)
   }); */ //commented by biju
 
+assistant.ask('hiyya');
+    
+// /*  
+ var webhookReply = 'Hello asdf.'
+
    res.status(200).json({
     source: 'webhook',
     speech: webhookReply,
-    displayText: webhookReply
-}) //added by biju
+    displayText: 'webhookReply'
+}) 
+   // */ //added by biju
 
   }
 
@@ -63,6 +97,8 @@ app.post('/', function (req, res) {
 
   assistant.handleRequest(actionMap);
 });
+
+*/
 
 if (module === require.main) {
   let server = app.listen(process.env.PORT || 8080, function () {
